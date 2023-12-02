842d2a0a76e973e3af8ff144d73d545fe8003056
IvMysl <IvMysl_2009@gmail.com>
2023-12-02 17:40:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+// Оголошення змінних:
+// let - змінна якій в поадльшому можна присвоїти нове значення
+// const - змінна, яка має стале значення, не можна переприсвоїти
+// var
+
+// let age = 5;
+// console.log(age);
+// age = 10;
+// console.log(age);
+
+// const age = 5;
+// console.log(age);
+// age = 10;
+// console.log(age);
+
+// let username; //невизачена змінна
+// console.log(username)
+
+// примітивні типи даних
+
+//Number - цілі числа і числа з плаваючою комою
+// String - рядок який починається одинарними ', aбо " лапками
+// const string = "string"
+//Boolean - логічний тип, має тільки 2 значення true or false
+// console.log(true===1);
+//null - значення яке означає ніщо.
+//undefined - значення не визначене
+
+// оператор typeof - вказує тип даних
+// let username;
+// console.log(typeof username);
+
+// Виведення даних
+// console.log('Виводимо дані в консоль');
+// alert('Виводимо дані на екран');
+
+//Отримання даних
+// confirm() - виводить модальне вікно з повідомленям і дві копки ok(true) and cancel(false)
+// const isComing = confirm("Будь ласка підтвердіть");
+// console.log(isComing);
+//prompt() = Виводить модальне вікно з полем вводу і кнопками ok(те що ввів користувач) і Cancel(null)
+
+// const value = prompt("Будь ласка введіть номер готелю");
+// console.log(typeof value);
+// console.log(value);
+
+//Основні оператори
+// математичні оператори
+// const x = 5;
+// const y = 5;
+// Додавання
+// console.log(x+y);
+// Відімання
+// console.log(x-y);
+//Множення
+// console.log(x*y);
+// ділення
+// console.log(x/y);
+// остача
+// console.log(x%y);
+// x and y - операнди, */+- - оператори
+
+// Оператори порівняння
+
+//a>b i a<b - більше/менше
+//a>=b i a=<b - більше/менше або дорівнює
+//a==b - рівність
+//a!=b - нерівність
+//a===b - сувора рівність
+// a!==b - сувора нерівність
+// console.log(5 == "5");буде true
+// console.log(5 === "5");буде false
